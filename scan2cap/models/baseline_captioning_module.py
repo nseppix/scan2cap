Add gradient clipping option. Fix loss helper. Minor changes to baseline and backbone.

scan2cap/models/baseline_captioning_module.py
@@ -33,7 +33,8 @@ class Decoder(nn.Module):
         self.decoder_dim = decoder_dim
         self.n_closest = n_closest
         self.dropout = dropout
-        
+
+        print(self.__dict__)
 
         self.vocab_size = len(vocab_list)
         embedding_dict["<end>"] = np.zeros(self.embed_dim)
@@ -96,6 +97,7 @@ class Decoder(nn.Module):
             max_distances = torch.max(torch.topk(distance, self.n_closest, dim=-1, largest=False)[0], dim=1, keepdim=True)[0]
             distance_mask = distance <= max_distances
             object_mask &= distance_mask
+            data_dict["object_mask"] = object_mask
             has_objects = torch.any(object_mask, dim=1)
             # print(torch.softmax(data_dict["objectness_scores"], dim=-1))
             vote_features = objectness.new_zeros((batch_size, self.vote_dimension))

scan2cap/scripts/train_scan2cap.py
@@ -73,7 +73,7 @@ def get_solver(args, dataloader, stamp):
     model = get_model(args)
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wd)
     vocabulary = VOCABULARY 
-    solver = SolverCaptioning(model, DC, dataloader, optimizer, stamp, vocabulary, args.use_attention, args.val_step , early_stopping=args.es, only_val=args.only_val)
+    solver = SolverCaptioning(model, DC, dataloader, optimizer, stamp, vocabulary, args.use_attention, args.val_step , early_stopping=args.es, only_val=args.only_val,gradient_clip=args.gradient_clip)
     if args.pnextractor_cp is not None:
         pnextractor_cp = torch.load(args.pnextractor_cp)
         model.load_pn_extractor(pnextractor_cp)
@@ -194,6 +194,7 @@ if __name__ == "__main__":
     parser.add_argument('--use_attention', action='store_true', help="Use attention for captioning, only works if votenet is used")
     parser.add_argument('--objectness_thresh', type=float, help="Threshold for accepting objects proposed by votenet", default=.75)
     parser.add_argument('--n_closest', type=int, help="Number of n closest votenet proposals are considered", default=32)
+    parser.add_argument('--gradient_clip', type=float, help="Clip gradients", default=None)
     args = parser.parse_args()
 
     # setting